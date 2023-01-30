f7d323cad36a9080504c605b5230b0bfbad97cc7
bob <bob.dev.oak@gmail.com>
2023-01-30 15:41:08 -0800

    fix project id in  cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: 'myxx15',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl:"http://localhost:4200",
-    projectId:"ng-raffle"
+    projectId:"myxx15"
   },
 });
