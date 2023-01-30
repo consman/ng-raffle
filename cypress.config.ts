a7bcec241cc53fb218da4e2d74a7b38acfeeb69f
bob <bob.dev.oak@gmail.com>
2023-01-30 14:55:08 -0800

    fix malformed cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,5 @@
 import { defineConfig } from "cypress";
 
-module.exports = {
-  projectId: "ng-raffle",
-  // The rest of the Cypress config options go here...
-}
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
